1a1b23b80d95a9414b561f666ca1ee43ef161010
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2019-04-07 07:35:50 +0100

    removing unneccasary file

--- test.js
+++
@@ -1,6 +0,0 @@
-var assert = require("assert");
-
-var Divider = require("./div.js");
-
-assert.strictEqual(new Divider(5, -6).result.quotient, -2);
-assert.strictEqual(new Divider(5, -6).result.remainder, 4, "Remainder failed");
